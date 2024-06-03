6fb8c05f13be10d073c7bb2a2bdfadf16194018b
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-03 20:59:55 -0300

    Poner un código de ejemplo en Main para la conexión a base de datos.

--- src/main/java/ar/unrn/tp4/ejercicio1/main/Main.java
+++ src/main/java/ar/unrn/tp4/ejercicio1/main/Main.java
@@ -1,10 +1,15 @@
 package ar.unrn.tp4.ejercicio1.main;
 
+import ar.unrn.tp4.ejercicio1.db.RegistroDeParticipantesJDBC;
+import ar.unrn.tp4.ejercicio1.dominio.DefaultSistemaDeParticipantes;
+import ar.unrn.tp4.ejercicio1.dominio.ServicioExterno;
+import ar.unrn.tp4.ejercicio1.dominio.SistemaDeParticipantes;
 import ar.unrn.tp4.ejercicio1.gui.VentanaAgregarParticipante;
 
 public class Main {
 	public static void main(String[] args) {
-		new VentanaAgregarParticipante((n, t, r) -> {
-		});
+		ServicioExterno servicio = new RegistroDeParticipantesJDBC("jdbc:mariadb://raspberrypi.local", "pruebas", "pruebas0");
+		SistemaDeParticipantes sistema = new DefaultSistemaDeParticipantes(servicio);
+		new VentanaAgregarParticipante(sistema);
 	}
 }

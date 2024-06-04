e7fea08e453f65286cf40a41799df0dfa988e1c5
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-04 19:39:38 -0300

    Limpiar comentarios de RegistroDeParticipantesJDBC.

--- src/main/java/ar/unrn/tp4/ejercicio1/db/RegistroDeParticipantesJDBC.java
+++ src/main/java/ar/unrn/tp4/ejercicio1/db/RegistroDeParticipantesJDBC.java
@@ -13,9 +13,6 @@ public class RegistroDeParticipantesJDBC implements ServicioExterno {
 	private Connection dbConn;
 
 	public RegistroDeParticipantesJDBC(String url, String user, String password) {
-//		String url = "jdbc:mariadb://raspberrypi.local/oo2tp4e1_participantes";
-//		String user = "pruebas";
-//		String password = "pruebas0";
 		try {
 			this.dbConn = DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
@@ -28,7 +25,6 @@ public class RegistroDeParticipantesJDBC implements ServicioExterno {
 		try (PreparedStatement st = dbConn.prepareStatement("insert into participantes(nombre, telefono, region) values(?,?,?)");) {
 			st.setString(1, participante.nombre().toString());
 			st.setString(2, participante.telefono().toString());
-			// TODO
 			st.setString(3, participante.region().mostrar());
 			st.executeUpdate();
 		} catch (SQLException e) {

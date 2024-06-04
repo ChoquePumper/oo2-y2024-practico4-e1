8b13f200539efb98471b1267cadc185389e2f294
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-03 21:50:44 -0300

    Los parámetros de configuración de conexión a base de datos se movió a un archivo separado.

--- src/main/java/ar/unrn/tp4/ejercicio1/main/Main.java
+++ src/main/java/ar/unrn/tp4/ejercicio1/main/Main.java
@@ -6,9 +6,19 @@ import ar.unrn.tp4.ejercicio1.dominio.ServicioExterno;
 import ar.unrn.tp4.ejercicio1.dominio.SistemaDeParticipantes;
 import ar.unrn.tp4.ejercicio1.gui.VentanaAgregarParticipante;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-	public static void main(String[] args) {
-		ServicioExterno servicio = new RegistroDeParticipantesJDBC("jdbc:mariadb://raspberrypi.local", "pruebas", "pruebas0");
+	public static void main(String[] args) throws IOException {
+		// Carga propiedades desde un archivo
+		Properties prop = new Properties();
+		prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+
+		String url = prop.getProperty("jdbc.url");
+		String user = prop.getProperty("jdbc.user"), password = prop.getProperty("jdbc.password");
+
+		ServicioExterno servicio = new RegistroDeParticipantesJDBC(url, user, password);
 		SistemaDeParticipantes sistema = new DefaultSistemaDeParticipantes(servicio);
 		new VentanaAgregarParticipante(sistema);
 	}
